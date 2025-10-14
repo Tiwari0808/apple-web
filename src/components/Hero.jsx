cc46c9b8ed898bf7601219681c7a93fe924fa8ac
Divyanshu <divyanshutiwari173@gmail.com>
2025-10-14 10:15:11 +0530

    completed hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section id='hero'>
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
+
+            <video src='/videos/hero.mp4' autoPlay muted playsInline />
+
+            <button className='mx-auto bg-[#0071E3] text-white rounded-full px-5 py-2'>Buy</button>
+
+            <p className='text-center'>From $1599 or $133/mo for 12 months</p>
+
+        </section>
+    )
+}
+
+export default Hero
