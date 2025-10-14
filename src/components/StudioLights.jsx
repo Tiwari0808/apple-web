24d1c15a120972a64251cf98c5d2c719e90842a9
Divyanshu <divyanshutiwari173@gmail.com>
2025-10-14 15:18:08 +0530

    building ProductView page

--- src/components/StudioLights.jsx
+++ src/components/StudioLights.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Environment, Lightformer } from "@react-three/drei";
+import React from "react";
 
 const StudioLights = () => {
   return (
-    <div>StudioLights</div>
-  )
-}
+    <group name="lights">
+      <Environment resolution={256}>
+        <group>
+          <Lightformer
+            form="rect"
+            intensity={10}
+            position={[-10, 5, -5]}
+            scale={10}
+            rotation-y={Math.PI/2}
+          />
+          <Lightformer
+            form="rect"
+            intensity={10}
+            position={[10, 0, 1]}
+            scale={10}
+            rotation-y={Math.PI/2}
+          />
+        </group>
+      </Environment>
+      <spotLight
+      angle={0.15}
+      decay={0}
+      intensity={Math.PI*0.2}
 
-export default StudioLights
+      />
+
+      
+    </group>
+  );
+};
+
+export default StudioLights;
