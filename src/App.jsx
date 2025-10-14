8eec700a9a4b60a91b5d8223aceac4264f2a2d74
Divyanshu <divyanshutiwari173@gmail.com>
2025-10-14 16:55:12 +0530

    completed ProductView section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -5,15 +5,18 @@ import { Canvas } from '@react-three/fiber'
 import { Box, OrbitControls } from '@react-three/drei'
 import MacbookModel14 from './models/Macbook-14'
 import StudioLights from './StudioLights'
+import ModelSwitcher from './three/ModelSwitcher'
+import { useMediaQuery } from 'react-responsive'
 
 const ProductViewer = () => {
-    let { color, scale, setColor, setScale } = useMacbookStore()
+    let { color, scale, setColor, setScale } = useMacbookStore();
+    const isMobile = useMediaQuery({ query: '(max-width:1024px)' })
     return (
         <section id='product-viewer'>
             <h2>Take a closer look</h2>
 
             <div className='controls'>
-                <p className='info'>MacbookPro 16" in {color}</p>
+                <p className='info'>MacbookPro | Available in 14" and 16" in Space Gray and Dark Blue Colors</p>
 
                 <div className='flex-center gap-5 mt-5'>
                     <div className="color-control">
@@ -28,13 +31,15 @@ const ProductViewer = () => {
                 </div>
             </div>
 
+
+
             <Canvas id='canvas' camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
-                {/* <Box position={[0, 0, 0]} scale={10*scale} material-color={color}></Box> */}
 
                 <StudioLights />
 
-                <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-                <OrbitControls enableZoom={false} />
+                {/* <MacbookModel14 scale={0.06} position={[0, 0, 0]} /> */}
+
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
             </Canvas>
         </section>
 
