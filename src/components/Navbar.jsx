f37888ec896f69e5133806acfc6fdd7b69762861
Divyanshu <divyanshutiwari173@gmail.com>
2025-10-14 09:33:17 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div>
+                <img src="/logo.svg" alt="logo" />
+            </div>
+            <ul>
+                {[
+                    {label:'Store'},
+                    {label:'Mac'},
+                    {label:'iPhone'},
+                    {label:'Watch'},
+                    {label:'Vision'},
+                    {label:'AirPods'}
+                ].map(({label})=>(
+                    <li key={label}>
+                        <a href={label}>{label}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className='flex-center gap-3'>
+                <button><img src="/search.svg" alt="search" /></button>
+                <button><img src="/cart.svg" alt="cart" /></button>
+            </div>
+             
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
