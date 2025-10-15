0f3cb594a57eb5be518032767710ca9995983659
Divyanshu <divyanshutiwari173@gmail.com>
2025-10-15 11:57:02 +0530

    'ShowCase section done'

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
+import ShowCase from './components/ShowCase'
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -14,6 +15,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <ProductViewer/>
+      <ShowCase/>
     </>
   )
 }
